d4ff652ecf772ad4295415a62588abbd21ea4370
Mike Webster <emailmikewebster@gmail.com>
2022-11-05 17:34:22 -0500

    WIP :bomb: good luck

--- utils.js
+++ utils.js
@@ -27,7 +27,41 @@ async function createIssue (title, body, repo, owner, token, options) {
         if (!resp.ok) {
             throw `unhappy response from github: ${JSON.stringify(resp)}`;
         }
-        
+
+        console.log(`issue created: ${(await resp.json()).html_url}`)
+    } catch (ex) {
+        console.log(`utils.createIssue::error: ${ex}`);
+    }
+}
+
+async function addIssueToProject() {
+    try {
+        const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
+        const headers =  {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+
+        var body = {
+            title,
+            body,
+        }
+
+        if (options.labels) {
+            body['labels'] = options.labels;
+        }
+
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(body)
+        });
+
+        if (!resp.ok) {
+            throw `unhappy response from github: ${JSON.stringify(resp)}`;
+        }
+
         console.log(`issue created: ${(await resp.json()).html_url}`)
     } catch (ex) {
         console.log(`utils.createIssue::error: ${ex}`);
@@ -37,3 +71,8 @@ async function createIssue (title, body, repo, owner, token, options) {
 module.exports = {
     createIssue
 }
+
+// leaving off
+// 1. struggling with some graphql shit
+//   - I need to figure out how to move the item to the desired column on the project board
+//   - it's a cumbersome process... youd think they'd  let you specify the  column  when you  create it.
